202e3905597356bd3f536127f124dcbb6719ca92
Colala65 <55476080+Colala65@users.noreply.github.com>
2022-10-13 14:40:41 +1100

    Add files via upload
    updated with early stopping, removed dropout

--- analysis/nn_exp.R
+++ analysis/nn_exp.R
@@ -2,20 +2,27 @@
 FLAGS <- flags(
   flag_integer("dense_units1", 32),
   flag_integer("dense_units2", 32),
-  flag_numeric("dropout1", 0.5),
-  flag_numeric("dropout2", 0.5),
-  flag_integer("batch_size", 1),
-  flag_numeric("learning_rate", 0.01),
-  flag_integer("decay_steps", 10)
+  flag_integer("dense_units3", 32),
+  flag_integer("dense_units4", 32),
+  #flag_numeric("dropout1", 0.25),
+  #flag_numeric("dropout2", 0.25),
+  #flag_numeric("dropout3", 0.25),
+  #flag_numeric("dropout4", 0.25),
+  flag_integer("batch_size", 32),
+  flag_numeric("learning_rate", 0.01)
 )
 
 # Model
 set.seed(2022)
 model <- keras_model_sequential() %>%
   layer_dense(units = FLAGS$dense_units1, activation = "relu") %>%
-  layer_dropout(rate = FLAGS$dropout1) %>%
+  #layer_dropout(rate = FLAGS$dropout1) %>%
   layer_dense(units = FLAGS$dense_units2, activation = "relu") %>%
-  layer_dropout(rate = FLAGS$dropout2) %>%
+  #layer_dropout(rate = FLAGS$dropout2) %>%
+  layer_dense(units = FLAGS$dense_units3, activation = "relu") %>%
+  #layer_dropout(rate = FLAGS$dropout3) %>%
+  layer_dense(units = FLAGS$dense_units4, activation = "relu") %>%
+  #layer_dropout(rate = FLAGS$dropout4) %>%
   layer_dense(1, activation = "linear")
 
 # LR scheduler
@@ -23,7 +30,10 @@ model <- keras_model_sequential() %>%
 
 
 # Compile w/ optimiser and loss
-model %>% compile(optimizer = optimizer_adam(learning_rate = FLAGS$learning_rate), loss = 'mean_absolute_error')
+model %>% compile(optimizer = optimizer_adam(learning_rate = FLAGS$learning_rate), loss = 'mean_absolute_error', metrics=c("mape"))
+
+# Early stopping
+early_stopping <- callback_early_stopping(monitor="loss", min_delta = 10, patience=6, mode="min")
 
 # Fit 
 set.seed(2022)
@@ -33,8 +43,8 @@ history <- model %>%
     y = as.matrix(y_train), 
     validation_split = 0.2, 
     batch_size = FLAGS$batch_size,
-    epochs = 40
+    epochs = 35,
+    callbacks = list(early_stopping)
   )
 
 plot(history)
-  
